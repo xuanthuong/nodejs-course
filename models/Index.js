324df8705e792d8af04e7070cae55d356ab496d4
ThuongTran <xuanthuongtran@gmail.com>
2017-10-23 22:45:52 +0700

    fix db_uri variable

--- models/Index.js
+++ models/Index.js
@@ -7,8 +7,9 @@ if (NODE_ENV == 'developement'){
     dotenv.load({path: path.join(__dirname, '..', '.env')})
 }
 
-process.env.DATABASE_URI = 'mysql://nodejs_course:123456@localhost:3306/nodejs_course'
-const sequelize = new Sequelize(process.env.DATABASE_URI)
+const db_uri = process.env.DATABASE_URI || 'mysql://nodejs_course_user:123789@localhost:3306/nodejs_course'
+
+const sequelize = new Sequelize(db_uri)
 
 const User = sequelize.import(path.join(__dirname, 'User.js'))
 const db = {}
