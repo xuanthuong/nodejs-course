e66d137ba7697fafd43d0fcfc321638b9fc7fdef
ThuongTran <xuanthuongtran@gmail.com>
2018-05-14 10:37:56 +0700

    done setting sequalize-oracle

--- models/Index.js
+++ models/Index.js
@@ -1,16 +1,15 @@
 const path = require('path')
 const dotenv = require('dotenv')
 const Sequelize = require('sequelize-oracle')
-// var dbConfig = require('../configs/db.js');
+var dbConfig = require('../configs/db.js');
 
 const NODE_ENV = process.env.NODE_ENV || 'developement'
 if (NODE_ENV == 'developement'){
     dotenv.load({path: path.join(__dirname, '..', '.env')})
 }
 
-// const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-const sequelize = new Sequelize('ocrl', 'C##ALGETA', 'C##ALGETA', {
-    host: '10.0.0.14',
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
     dialect: 'oracle',
     pool: {
         maxConnections: 100,
